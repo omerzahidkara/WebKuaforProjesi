a9ff4b6f45c68d33eaf24d5101e19e01285faaf9
omerzahidkara <83138216+omerzahidkara@users.noreply.github.com>
2024-12-28 18:44:19 +0300

    Entitiy düzenleme

--- OtoServisSatis.Entities/Musteri.cs
+++ OtoServisSatis.Entities/Musteri.cs
@@ -4,7 +4,7 @@ using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+// Musteri
 namespace OtoServisSatis.Entities
 {
     public class Musteri : IEntity
